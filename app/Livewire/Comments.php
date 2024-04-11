e23bb0a62bd94c7b37be89b116fee6b85c6688a2
Nahidul Islam <contact.nahidul@gmail.com>
2024-04-11 12:03:39 +0600

    Add form

--- app/Livewire/Comments.php
+++ app/Livewire/Comments.php
@@ -20,6 +20,10 @@ class Comments extends Component
 
     public function addComment()
     {
+        if ($this->newComment == '') {
+            return;
+        }
+
         array_unshift($this->comments, [
             'body' => $this->newComment,
             'created_at' => Carbon::now()->diffForHumans(),
